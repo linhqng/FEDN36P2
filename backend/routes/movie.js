77bc9e20173c61173f4e657597c7db96af771edc
DXT <dangxuanthangqt@gmail.com>
2020-11-03 13:57:41 +0700

    BE cinema/ showtime API

--- backend/routes/movie.js
+++ backend/routes/movie.js
@@ -2,6 +2,10 @@ var express = require("express");
 const {
   Create_movie,
   Upload_movie_photo,
+  Get_all_movie,
+  Get_movie_by_id,
+  Update_movie_by_id,
+  Delete_movie_by_id,
 } = require("../controllers/movieController/movieController");
 var router = express.Router();
 var auth = require("../middlewares/auth");
@@ -10,4 +14,14 @@ const upload = require("../middlewares/multer");
 router.post("/", auth.enhance, Create_movie);
 //upload image
 router.get("/photo/:id", upload("movies").single("file"), Upload_movie_photo);
+//Get all movie
+router.get("/", auth.enhance, Get_all_movie);
+//Get movie by id
+router.get("/:id", auth.enhance, Get_movie_by_id);
+//update movie by id
+router.patch("/:id", auth.enhance, Update_movie_by_id);
+//delete movie by id
+router.delete("/:id", auth.enhance, Delete_movie_by_id);
+
+
 module.exports = router;
